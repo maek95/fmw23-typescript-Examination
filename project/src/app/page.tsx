eaebc72b10bed4d5dc38c568b9debfe2bda1820b
maek95 <mekstrom01@gmail.com>
2024-11-11 10:37:37 +0100

    created next project

---
+++ project/src/app/page.tsx
@@ -0,0 +1,21 @@
+/* http://yumyum-assets.s3-website.eu-north-1.amazonaws.com
+
+// api generated:
+// https://fdnzawlcf6.execute-api.eu-north-1.amazonaws.com/keys
+yum-4wOFSa0vV0WtlFYK
+
+// tenant id generated:
+// https://fdnzawlcf6.execute-api.eu-north-1.amazonaws.com/tenants
+{
+  "id": "2ncs",
+  "name": "maek"
+}
+*/
+
+export default function Home() {
+  return (
+    <div>
+      <h1>hej</h1>
+    </div>
+  );
+}
