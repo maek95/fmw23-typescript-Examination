83dc2843a9611d8aae7e3c3a2f138043307e05c9
maek95 <mekstrom01@gmail.com>
2024-11-14 17:29:30 +0100

    localhost url

--- project/src/utils/utils.ts
+++ project/src/utils/utils.ts
@@ -1,3 +1,3 @@
-export const url = "https://food-truck-pied.vercel.app/"; // http://localhost:3000/
+export const url = "http://localhost:3000/"; //  https://food-truck-pied.vercel.app/     // I HAVE TO USE AN ENV VARIABLE IT SEEMS TO WORK ON VERCEL... I GUESS IT IS BUILDING WITH A DIFFERENT URL ??????????????
 export const apiKey = "yum-i0jmhtjgqKZhp6Hl";
 export const tenantId = "2ncs";
